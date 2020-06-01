bbff3f5a30f1719904f0082cdb1965335a6931fc
Anne Costa <anneizaura@gmail.com>
2020-06-01 18:09:26 -0300

    validates items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :category
   has_many :transactions
   has_many :reviews
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
 end
