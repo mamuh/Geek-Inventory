1623c1664ed4fc015606d4a677e6355ace234345
Mariana Caramori <musardo@mail.com>
2020-06-05 11:30:23 -0300

    removed role from users

--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,6 @@ ActiveRecord::Schema.define(version: 2020_06_04_205115) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "role", default: "buyer"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
