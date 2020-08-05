33190bc2c1a36eb04b82e2bb9dae4980a7a36d8a
Mariana Caramori <musardo@mail.com>
2020-08-05 18:26:10 -0300

    home page responsive

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,7 +26,7 @@ require("channels")
 import "bootstrap";
 
 import { loadDynamicBannerText } from '../components/banner';
-import { fadeIt } from '../components/background_fade';
+// import { fadeIt } from '../components/background_fade';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -34,5 +34,4 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   loadDynamicBannerText();
-  fadeIt();
 });
