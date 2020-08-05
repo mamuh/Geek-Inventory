57da4ca06ff342e7b0a327b06c8911ebba70dcab
Mariana Caramori <musardo@mail.com>
2020-08-05 17:50:52 -0300

    pagination with border radius

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -4,7 +4,7 @@ const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Geek Inventory"],
     typeSpeed: 100,
-    startDelay: 1000
+    startDelay: 700
   });
 }
 
